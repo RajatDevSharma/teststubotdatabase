8b53f8cb9621ee807c27b049da5d61986580b37d
RajatDevSharma <rajatdevsharma@gmail.com>
2017-12-16 16:08:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
 import os
-from flask import Flask
+import urllib
+import json
+import re
 import pymongo 
+
+from flask import Flask
+from flask import request
+from flask import make_response
+
 from pymongo import MongoClient
 
 app = Flask(__name__)
@@ -8,8 +15,62 @@ app = Flask(__name__)
 client = MongoClient( "mongodb://rajatdev:rdsharma@ds059207.mlab.com:59207/heroku_lgz52rzd")
 
 db = client.heroku_lgz52rzd
-collection = db.librarysample
 
+@app.route('/webhook', methods=['POST'])
+def webhook():
+    req = request.get_json(silent=True, force=True)
+
+    print("Request:")
+    print(json.dumps(req, indent=4))
+
+    res = makeWebhookResult(req)
+
+    res = json.dumps(res, indent=4)
+    print(res)
+    r = make_response(res)
+    r.headers['Content-Type'] = 'application/json'
+    return 
+
+def makeWebhookResult(req):
+    if req.get("result").get("action") == "search.book.title":
+        
+        collection = db.librarysample
+        
+        result = req.get("result")
+        parameters = result.get("parameters")
+        book = parameters.get("title")
+        
+        '''
+        #find_book="discrete"
+        speech=""
+        for i in book:
+            find_book=i
+        
+        matches = [x for x in books if re.search(find_book, x, re.M|re.I)]
+        
+        if len(matches)==0:
+            speech="Not available"
+        
+        for i in matches:
+            speech= speech + i + " , " 
+            
+        print("Response:")
+        print(speech)
+        '''
+        
+        a=[]
+        bookResult = collection.find( {"title":book} )
+        for i in bookResult:
+            a.append(i["title"])
+        
+        return {
+            "speech": a[0],
+            "displayText": a[0],
+            #"data": {},
+            # "contextOut": [],
+            "source": "python_stubot"
+        }
+'''
 @app.route("/")
 def start():
     a=[]
@@ -17,7 +78,7 @@ def start():
     for i in result:
         a.append(i["title"])
     return a[0]
-
+'''
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
